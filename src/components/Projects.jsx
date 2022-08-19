9ef14c38c4925756535a0124752f6f0995eac2e1
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2022-08-18 20:23:27 -0500

    fixed project wording

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -81,7 +81,7 @@ const Projects = () => {
                     </div>
                 </div>
                 <div style={projectText}><a style={projectLink} href="http://www.joshkotrous.io">joshkotrous.io</a></div>
-                <div style={projectDescription}>my first developer portfolio. Built with React and Python, hosted on Amazon S3, powered by AWS Lambda, and deployed with GitHub actions. </div>
+                <div style={projectDescription}>My first developer portfolio. Built with React and Python, hosted on Amazon S3, powered by AWS Lambda, and deployed with GitHub actions. </div>
             </div>
         </div>
 
