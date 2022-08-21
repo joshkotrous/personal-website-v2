3a17622ce4e6aa7a65bfeb40a7313282b7d7820e
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2022-08-20 20:22:52 -0500

    fixed vowels in welcome header

--- src/components/WelcomeHeader.jsx
+++ src/components/WelcomeHeader.jsx
@@ -33,8 +33,9 @@ const cyclingText = {
 
 const WelcomeHeader = () => {
     const [index, setIndex] = useState(0)
-
+    const [vowels] = useState(["a", "e", "i", "o"])
     const [headers]  = useState(["software", "automation", "react", "uipath", "python", "aws", "solution architecture"])
+    const [title, setTitle] = useState(headers[index])
 
 
     useEffect(()=> {
@@ -43,6 +44,7 @@ const WelcomeHeader = () => {
             if (index === headers.length - 1) {
                 setIndex(0);
             }
+            setTitle(headers[index])
           }, 2000)
       
         return (() => clearInterval(interval)) 
@@ -57,7 +59,7 @@ const WelcomeHeader = () => {
               <div style={{ display: 'flex', flexDirection: 'column', }}>
                   <div style={{ background: 'white', width: '100%', height: '1px', marginLeft: 'auto', marginRight: 'auto', marginBottom: '32px' }}></div>
                   <div style={headerTextContainer}>
-                      <h1 style={{ fontWeight: '300' }}>a <br></br><span style={cyclingText}>{headers[index]}</span><br></br> enthusiast</h1>
+                      <h1 style={{ fontWeight: '300' }}>{vowels.includes(title.substring(0,1)) ? "an" : "a"}<br></br><span style={cyclingText}>{title}</span><br></br> enthusiast</h1>
                   </div>
                   <div style={{ background: 'white', width: '100%', height: '1px', marginLeft: 'auto', marginRight: 'auto', marginTop: '32px' }}></div>
               </div>
