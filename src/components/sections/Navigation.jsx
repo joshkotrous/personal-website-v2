f27e289e15e22c81e727b673a0ad1776474be983
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2022-09-02 20:29:00 -0500

    disabled hamburger for mobile
    q
    :

--- src/components/sections/Navigation.jsx
+++ src/components/sections/Navigation.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-scroll";
 import PropTypes from "prop-types";
 import { ReactComponent as Hamburger } from "../../assets/hamburger.svg";
 import "../../assets/navigation.css";
+import { useEffect } from "react";
 
 const Navigation = (props) => {
   Navigation.propTypes = {
@@ -27,6 +28,8 @@ const Navigation = (props) => {
       url: "./#contact",
     },
   ];
+  const [width, setWidth] = useState(window.innerWidth);
+  const [isMobile, setIsMobile] = useState(false);
   const menuClick = () => {
     if (props.showMenu) {
       props.setShowMenu(false);
@@ -35,17 +38,51 @@ const Navigation = (props) => {
     }
   };
 
+  useEffect(() => {
+    const updateWindowDimensions = () => {
+      setWidth(window.innerWidth);
+      setIsMobile(width <= 768);
+    };
+    window.addEventListener("resize", updateWindowDimensions);
+
+    return () => {
+      window.removeEventListener("resize", updateWindowDimensions);
+    };
+  }, []);
+
   return (
     <div className="navContainer">
       <div className="headerContainer">
         <h1>josh</h1>
-        <div>
-          <Hamburger onClick={menuClick} />
-        </div>
+        {isMobile ? (
+          <div>
+            <Hamburger onClick={menuClick} />
+          </div>
+        ) : (
+          <div style={{ display: "flex" }}>
+            {" "}
+            {navOptions.map((item) => {
+              return (
+                <div key={item.text} className="navOptionContainer">
+                  <Link
+                    className="navOption"
+                    to={item.text}
+                    smooth={true}
+                    duration={300}
+                  >
+                    {item.text}
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
       <div
         className={
-          props.showMenu ? "navOptionsContainer" : "navOptionsContainer-hidden"
+          props.showMenu && !isMobile
+            ? "navOptionsContainer"
+            : "navOptionsContainer-hidden"
         }
       >
         {navOptions.map((item) => {
