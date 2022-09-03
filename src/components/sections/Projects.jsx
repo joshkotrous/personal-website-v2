66f32722b8b9c45ddf23857cebbccb319b984810
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2022-09-03 18:53:34 -0500

    refined al margins and spacing

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import { React } from "react";
 import "../../assets/projects.css";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Projects = () => {
   const projectsList = [
@@ -8,31 +10,41 @@ const Projects = () => {
       url: "https://joshkotrous.io",
       description:
         "My first developer portfolio. Built with React and Python, hosted on Amazon S3, powered by AWS Lambda, and deployed with GitHub actions.",
+      delay: 0.25,
     },
   ];
+  const { ref, inView } = useInView();
 
   return (
-    <div style={{ marginTop: "10%" }}>
-      {projectsList.map((item, index) => {
-        return (
-          <div key={index + "-project"} className="projectContainer">
-            <div className="projectFrameContainer">
-              <div style={{ transform: "scale(0.4)" }}>
-                <iframe
-                  title={item.title}
-                  scrolling="no"
-                  className="projectFrame"
-                  src={item.url}
-                />
-              </div>
-            </div>
-            <div className="projectText">
-              <a href={item.url}>{item.title}</a>
-            </div>
-            <div className="projectDescription">{item.description}</div>
-          </div>
-        );
-      })}
+    <div ref={ref} className="projectsContainer">
+      {inView
+        ? projectsList.map((item, index) => {
+            return (
+              <motion.div
+                initial={{ y: -50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.75, delay: item.delay }}
+                key={index + "-project"}
+                className="projectContainer"
+              >
+                <div className="projectFrameContainer">
+                  <iframe
+                    title={item.title}
+                    scrolling="no"
+                    className="projectFrame"
+                    src={item.url}
+                  />
+                </div>
+                <div>
+                  <div className="projectText">
+                    <a href={item.url}>{item.title}</a>
+                  </div>
+                  <div className="projectDescription">{item.description}</div>
+                </div>
+              </motion.div>
+            );
+          })
+        : null}
     </div>
   );
 };
